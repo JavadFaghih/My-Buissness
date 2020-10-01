39b1e4867209e7ce6e642b7abe27a9d205870c08
Javad Faghih <javad.4145@gmail.com>
2020-10-01 14:43:57 +0330

    Initial Commit

---
+++ My Buissness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Buissness
+//
+//  Created by javad faghih on 7/10/1399 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
